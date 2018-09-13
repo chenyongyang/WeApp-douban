0d1cfd30b33808faac484c639a01ee4580f2b46c
ChenYongYang <1214137301@qq.com>
2018-09-13 10:04:07 +0800

    封装调用豆瓣api的方法

---
+++ utils/douban.js
@@ -0,0 +1,19 @@
+const URI = 'https://douban.uieee.com/v2/movie'
+const fetch = require('./fetch')
+
+function fetchApi(type, params) {
+    return fetch(URI, type, params)
+}
+
+function find(type, page = 1, count = 20, search = '') {
+    const params = { start: (page - 1) * count, count: count, city: getApp().data.currentCity }
+    return fetchApi(type, search ? Object.assign(params, { q: search }) : params)
+        .then(res => res.data)
+}
+
+function findOne(id) {
+    return fetchApi('subject/' + id)
+        .then(res => res.data)
+}
+
+module.exports = { find, findOne }
