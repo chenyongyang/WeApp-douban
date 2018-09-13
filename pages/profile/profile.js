87a282304d8495d98e4b12a59da5ee43a7c81166
ChenYongYang <1214137301@qq.com>
2018-09-13 10:33:43 +0800

    个人信息页渲染真实数据

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,1 +1,35 @@
-Page({})
+// 获取全局应用程序实例对象
+const app = getApp()
+
+// 创建页面实例对象
+Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        title: 'About',
+        userInfo: {
+            nickName: 'https://github.com/chenyongyang',
+            avatarUrl: '../../images/avatar.png'
+        }
+    },
+
+    getUserInfo() {
+        app.wechat.getUserInfo()
+            .then(res => this.setData({ userInfo: res.userInfo }))
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad() {
+        app.wechat.login()
+            .then(res => {
+                if (res.code) {
+                    console.log('登录成功！' + res.code)
+                } else {
+                    console.error('获取用户登录态失败！' + res.errMsg)
+                }
+            })
+    }
+})
