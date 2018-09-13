5cf3cfd24be32cb09d8c39f34ae1bce50d660f7d
ChenYongYang <1214137301@qq.com>
2018-09-13 10:29:20 +0800

    列表页渲染真实数据

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,66 +1,71 @@
-// pages/list/list.js
+// 获取全局应用程序实例对象
+const app = getApp()
+
+// 创建页面实例对象
 Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        title: '',
+        subtitle: '加载中...',
+        type: 'in_theaters',
+        hasMore: true,
+        page: 1,
+        size: 20,
+        movies: []
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    loadMore() {
+        if (!this.data.hasMore) return
 
-  },
+        wx.showLoading({ title: '正在加载中...' })
+        this.setData({ subtitle: '加载中...' })
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+        return app.douban.find(this.data.type, this.data.page++, this.data.size)
+            .then(d => {
+                if (d.subjects.length) {
+                    this.setData({ subtitle: d.title, movies: this.data.movies.concat(d.subjects) })
+                } else {
+                    this.setData({ subtitle: d.title, hasMore: false })
+                }
+                wx.hideLoading()
+            })
+            .catch(e => {
+                this.setData({ subtitle: '获取数据异常' })
+                console.error(e)
+                wx.hideLoading()
+            })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(params) {
+        this.data.title = params.title || this.data.title
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+        this.data.type = params.type || this.data.type
 
-  },
+        this.loadMore()
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+        wx.setNavigationBarTitle({ title: this.data.title + ' « 电影 « 豆瓣' })
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+        this.setData({ movies: [], page: 1, hasMore: true })
+        this.loadMore()
+            .then(() => wx.stopPullDownRefresh())
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    onReachBottom() {
+        this.loadMore()
+    }
 })
