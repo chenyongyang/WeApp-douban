a247fe4508987c3b342bcf36e6c653cdd44d0537
ChenYongYang <1214137301@qq.com>
2018-09-13 10:21:05 +0800

    榜单页渲染真实数据

--- pages/board/board.js
+++ pages/board/board.js
@@ -1,1 +1,30 @@
-Page({})
+const app = getApp()
+
+Page({
+    data: {
+        boards: [
+            { key: 'in_theaters' },
+            { key: 'coming_soon' },
+            { key: 'new_movies' },
+            { key: 'top250' }
+        ]
+    },
+
+    onLoad() {
+        wx.showLoading({ title: '正在加载中...' })
+
+        const tasks = this.data.boards.map(board => {
+            return app.douban.find(board.key, 1, 8)
+                .then(d => {
+                    board.title = d.title
+                    board.movies = d.subjects
+                    return board
+                })
+        })
+
+        Promise.all(tasks).then(boards => {
+            this.setData({ boards: boards, loading: false })
+            wx.hideLoading()
+        })
+    }
+})
