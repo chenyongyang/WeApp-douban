da3d4996e11d161bd808670d5e56e618fa82b132
ChenYongYang <1214137301@qq.com>
2018-09-13 10:38:46 +0800

    详情页渲染真实数据

--- pages/item/item.js
+++ pages/item/item.js
@@ -1,66 +1,47 @@
-// pages/item/item.js
+// 获取全局应用程序实例对象
+const app = getApp()
+
+// 创建页面实例对象
 Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        title: '',
+        movie: {}
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(params) {
+        wx.showLoading({ title: '正在加载中...' })
 
-  },
+        app.douban.findOne(params.id)
+            .then(d => {
+                this.setData({ title: d.title, movie: d })
+                wx.setNavigationBarTitle({ title: d.title + ' « 电影 « 豆瓣' })
+                wx.hideLoading()
+            })
+            .catch(e => {
+                this.setData({ title: '获取数据异常', movie: {} })
+                console.error(e)
+                wx.hideLoading()
+            })
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+        wx.setNavigationBarTitle({ title: this.data.title + ' « 电影 « 豆瓣' })
+    },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    onShareAppMessage() {
+        return {
+            title: this.data.title,
+            desc: this.data.title,
+            path: '/pages/item?id=' + this.data.id
+        }
+    }
 })
