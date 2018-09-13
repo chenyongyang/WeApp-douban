e9229652db95849197ba7a3b0c0e60e1aa5b59f0
ChenYongYang <1214137301@qq.com>
2018-09-13 10:16:53 +0800

    将所有api导入app.js

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-//app.js
-App({ // 这里放一些生命周期函数和全局数据对象
+const wechat = require('./utils/wechat.js')
+const douban = require('./utils/douban.js')
+const baiduMap = require('./utils/baiduMap.js')
+
+App({
+    data: {
+        name: 'Douban Movie',
+        version: '0.1.0',
+        currentCity: '深圳'
+    },
+    wechat: wechat,
+    douban: douban,
+    baiduMap: baiduMap,
+    onLaunch() {
+        wechat
+            .getLocation()
+            .then(res => {
+                const { latitude, longitude } = res
+                return baiduMap.getCityName(latitude, longitude)
+            })
+            .then(name => {
+                this.data.currentCity = name.replace('市', '')
+                console.log(`currentCity : ${this.data.currentCity}`)
+            })
+            .catch(err => {
+                this.data.currentCity = '北京'
+                console.error(err)
+            })
+    }
 })
